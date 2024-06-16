8c007d77e62ff9534d349962fee57014ac1438ac
anshika231106 <147130116+anshika231106@users.noreply.github.com>
2024-06-16 16:58:12 +0530

    script completed

--- script.js
+++ script.js
@@ -5,6 +5,25 @@ const expense=document.querySelector("#expense");
 const balance=document.querySelector("#balance");
 const history=document.querySelector("ul");
 
+function remove(ID)
+{
+    const bal=Number(document.getElementById(ID).firstElementChild.innerHTML);
+    
+    if(bal>0)
+    {
+        balance.innerHTML=Number(balance.innerHTML)-Math.abs(bal);
+        income.innerHTML=Number(income.innerHTML)-bal;
+    }
+    else{
+        balance.innerHTML=Number(balance.innerHTML)+Math.abs(bal);
+        expense.innerHTML=Number(expense.innerHTML)+bal;
+    }
+
+    const del=document.getElementById(ID);
+    del.parentNode.removeChild(del);
+
+}
+
 function updatevalues()
 {
     if(amt.value<0)
@@ -30,7 +49,9 @@ function add(e)
     else{
         const list=document.createElement("li");
         list.classList.add(amt.value<0?"minus":"plus");
-        list.innerHTML=text.value+"<span>"+amt.value+"</span><button class='delete-btn' onclick='removeTransaction()'>X</button>";
+        var ID=(Math.floor(Math.random()*100000));
+        list.setAttribute("id",ID);
+        list.innerHTML=text.value+"<span>"+amt.value+"</span><button class='delete-btn' onclick='remove("+ID+")'>X</button>";
         history.appendChild(list);
         updatevalues();
 
